18b58dee3f7fd6e3f2720037e4c3affc970fdf62
Kuba <jakub.zmidzinski@gmail.com>
2020-04-03 18:33:53 +0200

    Refactored header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,23 @@
 import React from "react"
-import avatar from "../../content/images/avatar.png"
+import Image from "gatsby-image"
 
 const Header = ({ title }) => {
+  const data = useStaticQuery(graphql`
+  query AvatarQuery {
+    avatar: file(absolutePath: { regex: "../../content/images/avatar.png" }) {
+      childImageSharp {
+        fixed(width: 64, height: 64) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`)
+
   return (
     <div className="brand">
         <a className="" href="/">
-          <img src={avatar} className="avatar" />
+          <Image fixed={data.avatar.childImageSharp.fixed} className="avatar" />
           <span className="text">{title}</span>
         </a>
     </div>
