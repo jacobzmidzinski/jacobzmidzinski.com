eed8d1640e6ebd40be05a92ff457b7d254948663
Kuba <jakub.zmidzinski@gmail.com>
2020-04-03 18:44:26 +0200

    Fixed header.js graphql query

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,30 @@
 import React from "react"
 import Image from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Header = ({ title }) => {
-  const data = useStaticQuery(graphql`
-  query AvatarQuery {
-    avatar: file(absolutePath: { regex: "../../content/images/avatar.png" }) {
-      childImageSharp {
-        fixed(width: 64, height: 64) {
-          ...GatsbyImageSharpFixed
+  const {
+    placeholderImage: {
+      childImageSharp: { fixed },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "avatar.png" }) {
+        childImageSharp {
+          fixed(width: 40) {
+            ...GatsbyImageSharpFixed_noBase64
+          }
         }
       }
     }
-  }
-`)
+  `)
 
   return (
     <div className="brand">
-        <a className="" href="/">
-          <Image fixed={data.avatar.childImageSharp.fixed} className="avatar" />
-          <span className="text">{title}</span>
-        </a>
+      <a className="" href="/">
+        <Image fixed={fixed} className="avatar" />
+        <span className="text">{title}</span>
+      </a>
     </div>
   )
 }
